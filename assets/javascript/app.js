28154d056805043e7af7694ea5b1419a0ea1148c
Grimble <grimblelewisthethird@gmail.com>
2019-03-22 01:32:41 -0400

    added mobile scaling and additonal details about the images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -37,20 +37,25 @@ function displayAnimalGifs() {
     console.log(response);
     var results = response.data;
 
+    // this for loop goes through the results length (which is limited to 10) and adds a class of gif and multiple attributes to help
+    // control the gifs and adds details that appear on the page
     for (var i = 0; i < results.length; i++) {
       var animalDiv = $("<div>");
-      var p = $("<p>").text(`Rating: ${results[i].rating}`);
+      var p = $("<p>").append(`
+      Title: ${results[i].title.toUpperCase()}</br>
+      Rating: ${results[i].rating.toUpperCase()}`);
       var animalImage = $("<img>");
       animalImage.addClass("gif");
       animalImage.attr("data-state", "animate");
       animalImage.attr("animate", results[i].images.fixed_height.url);
       animalImage.attr("still", results[i].images.fixed_height_still.url);
       animalImage.attr("src", results[i].images.fixed_height.url);
-      animalDiv.append(p);
       animalDiv.append(animalImage);
+      animalDiv.append(p);
       $("#animal-gifs").prepend(animalDiv);
     }
 
+    // when a gif is clicked it will change its src to the still image and when clicked again will switch it back to the animated src
     $(".gif").on("click", function() {
       console.log(this);
       var state = $(this).attr("data-state");
@@ -65,6 +70,8 @@ function displayAnimalGifs() {
   });
 }
 
+// this function is run when the page loads as well as when user submits input into the 'add an animal' form
+// it gives each button a class, an attribute, and the displays the text that was put into the form
 function renderButtons() {
   $("#buttons-view").empty();
 
@@ -77,6 +84,8 @@ function renderButtons() {
   }
 }
 
+// when the submit button is clicked and has text in it it will create a button that can be clicked to create gifs
+// if no text is entered the submit button will not add a button
 $("#add-animal").on("click", function(event) {
   event.preventDefault();
 
@@ -84,11 +93,17 @@ $("#add-animal").on("click", function(event) {
     .val()
     .trim();
 
-  animals.push(animal);
+  if (animal === "") {
+    return;
+  } else {
+    animals.push(animal);
 
-  renderButtons();
+    renderButtons();
+  }
 });
 
+// when anything with the class of 'animal-btn' is clicked, displayAnimalGifs runs
 $(document).on("click", ".animal-btn", displayAnimalGifs);
 
+//creates buttons from the base animal array when page loads
 renderButtons();
